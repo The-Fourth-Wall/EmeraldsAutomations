e697a249f34e19ef44c487b3dc4ab9869fe31e8d
Oblivious-Oblivious <me262thanos@gmail.com>
2020-06-25 02:17:28 +0300

    Create includes.h

---
+++ headers/includes.h
@@ -0,0 +1,9 @@
+#ifndef __INCLUDES_H_
+#define __INCLUDES_H_
+
+#include <stdlib.h> /* malloc, calloc, realloc, free */
+#include <setjmp.h> /* jmp_bug, setjmp */
+#include <stdarg.h> /* va_start, va_end, va_arg */
+#include <stdio.h> /* printf */
+
+#endif
