c8f69d0208006ac278c1d3938fb3daed9deb9b24
Oblivious <me262thanos@gmail.com>
2021-04-08 20:43:31 +0300

    Removed includes used for cSpec.

--- src/cAutomations.h
+++ src/cAutomations.h
@@ -6,7 +6,5 @@
 #include "cAutomations/headers/setup_definitions.h"
 #include "cAutomations/headers/standard_string.h"
 #include "cAutomations/headers/hash_functions.h"
-#include "cAutomations/headers/standard_boolean.h"
-#include "cAutomations/headers/syntactic_sugar.h"
 
 #endif
